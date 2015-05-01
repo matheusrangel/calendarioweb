2e7caa98a685131bd6d66fe0a4fcb8166b34c91a
Matheus Rangel <matheusulysses159@gmail.com>
2015-05-01 20:12:25 -0300

    Iniciado mapeamento

--- src/br/edu/ifpb/calendario/models/Anotacao.java
+++ src/br/edu/ifpb/calendario/models/Anotacao.java
@@ -2,10 +2,25 @@ package br.edu.ifpb.calendario.models;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Anotacao {
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column
+	@Temporal(TemporalType.DATE)
 	private Date data;
+	
+	@Column
 	private String mensagem;
 	
 	public Long getId() {
--- src/br/edu/ifpb/calendario/models/Usuario.java
+++ src/br/edu/ifpb/calendario/models/Usuario.java
@@ -1,9 +1,31 @@
 package br.edu.ifpb.calendario.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Columns;
+import org.hibernate.annotations.Type;
+
+@Entity
 public class Usuario {
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-	private String login, nome, senha;
+	
+	@Column(length=30)
+	private String login;
+	
+	@Column(length=30)
+	private String nome;
+	
+	@Column(length=30)
+	private String senha;
+	
+	@Type(type="true_false")
+	@Column
 	private Boolean admin;
 	
 	public Long getId() {
