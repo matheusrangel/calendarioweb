d1b714752525657b888e2de8b1c3c84a08c53c6a
Victor Pereira <victordualib@gmail.com>
2015-05-02 17:51:38 -0300

    Merge

--- src/br/edu/ifpb/calendario/servlets/CalendarioServlet.java
+++ src/br/edu/ifpb/calendario/servlets/CalendarioServlet.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package br.edu.ifpb.calendario.servlets;
 
 import java.io.IOException;
@@ -45,8 +44,6 @@ public class CalendarioServlet extends HttpServlet {
 	public void login(HttpServletRequest request, HttpServletResponse response){
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		Usuario usuario = new Usuario();
-		
-		
 	}
 	
 	public void cadastro(HttpServletRequest request, HttpServletResponse response){
@@ -62,68 +59,3 @@ public class CalendarioServlet extends HttpServlet {
 	}
 
 }
-=======
-package br.edu.ifpb.calendario.servlets;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import br.edu.ifpb.calendario.dao.UsuarioDAO;
-import br.edu.ifpb.calendario.models.Usuario;
-
-@WebServlet("/calendario.do")
-public class CalendarioServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-    
-	
-    public CalendarioServlet() {
-        super();
-    }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String op = request.getParameter("op");
-		
-		switch (op) {
-		case "login":
-			login(request, response);
-			break;
-		
-		case "cadastro":
-			cadastro(request, response);
-			break;
-
-		default:
-			break;
-		}
-	}
-	
-	public void login(HttpServletRequest request, HttpServletResponse response){
-		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		Usuario usuario = new Usuario();
-		
-		
-	}
-	
-	public void cadastro(HttpServletRequest request, HttpServletResponse response){
-		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		Usuario usuario = new Usuario();
-		usuario.setLogin(request.getParameter("login"));
-		usuario.setNome(request.getParameter("nome"));
-		usuario.setSenha(request.getParameter("senha"));
-		usuarioDAO.begin();
-		usuarioDAO.persist(usuario);
-		usuarioDAO.commit();
-		usuarioDAO.close();
-	}
-
-}
->>>>>>> origin/master
