36c4fb0a4fcf21a7daf5e0befabf4ee4ab53c229
Matheus Rangel <matheusulysses159@gmail.com>
2015-05-02 21:21:06 -0300

    DAO modificado

--- src/br/edu/ifpb/calendario/dao/UsuarioDAO.java
+++ src/br/edu/ifpb/calendario/dao/UsuarioDAO.java
@@ -1,5 +1,9 @@
 package br.edu.ifpb.calendario.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
+
 import br.edu.ifpb.calendario.models.Usuario;
 
 public class UsuarioDAO extends GenericDAO<Usuario> {
@@ -7,5 +11,18 @@ public class UsuarioDAO extends GenericDAO<Usuario> {
 	public UsuarioDAO() {
 		super();
 	}
+
+	public List<Usuario> findAll() {
+		return findAll(Usuario.class);
+	}
+
+	public Usuario findByCodigo(String valor) {
+		return super.findByField(Usuario.class, "codigo", valor);
+	}
+
+	public Usuario findByLogin(String login){
+		return super.findByField(Usuario.class, "login", login);
+	}
+
 
 }
--- src/br/edu/ifpb/calendario/servlets/CalendarioServlet.java
+++ src/br/edu/ifpb/calendario/servlets/CalendarioServlet.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.calendario.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,24 +16,24 @@ import br.edu.ifpb.calendario.models.Usuario;
 @WebServlet("/calendario.do")
 public class CalendarioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	
-    public CalendarioServlet() {
-        super();
-    }
-   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+
+	public CalendarioServlet() {
+		super();
 	}
-	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String op = request.getParameter("op");
-		
+
 		switch (op) {
 		case "login":
 			login(request, response);
 			break;
-		
+
 		case "cadastro":
 			cadastro(request, response);
 			break;
@@ -41,24 +42,25 @@ public class CalendarioServlet extends HttpServlet {
 			break;
 		}
 	}
-	
+
 	public void login(HttpServletRequest request, HttpServletResponse response){
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		Usuario usuario = new Usuario();
 	}
-	
-	public void cadastro(HttpServletRequest request, HttpServletResponse response){
+
+	public void cadastro(HttpServletRequest request, HttpServletResponse response) {
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		Usuario usuario = new Usuario();
 		usuario.setLogin(request.getParameter("login"));
 		usuario.setNome(request.getParameter("nome"));
-		usuario.setSenha(request.getParameter("senha"));
-		usuarioDAO.begin();
+		usuario.setSenha(request.getParameter("senha"));	
+
+		usuarioDAO.findByLogin(usuario.getLogin());
 		usuarioDAO.persist(usuario);
-		usuarioDAO.commit();
 		usuarioDAO.close();
-		
-		
+
+
+
 	}
 
 }
