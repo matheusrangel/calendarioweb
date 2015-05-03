7aa1a6744cbd9fa357626a6b5826855ca55e603c
Matheus Rangel <matheusulysses159@gmail.com>
2015-05-03 01:14:45 -0300

    commit tosco

--- src/br/edu/ifpb/calendario/dao/UsuarioDAO.java
+++ src/br/edu/ifpb/calendario/dao/UsuarioDAO.java
@@ -37,3 +37,111 @@ public class UsuarioDAO extends GenericDAO<Usuario> {
 	}
 	
 }
+
+package br.edu.ifpb.calendario.dao;
+
+import java.util.List;
+
+import br.edu.ifpb.calendario.models.Usuario;
+
+public class UsuarioDAO extends GenericDAO<Usuario> {
+
+	public UsuarioDAO() {
+		super();
+	}
+
+	public List<Usuario> findAll() {
+		return findAll(Usuario.class);
+	}
+
+	public Usuario findByCodigo(String valor) {
+		return super.findByField(Usuario.class, "codigo", valor);
+	}
+
+	public Usuario findByLogin(String login){
+		return super.findByField(Usuario.class, "login", login);
+	}
+
+
+}
+
+package br.edu.ifpb.calendario.dao;
+
+import java.util.List;
+
+import br.edu.ifpb.calendario.models.Usuario;
+
+public class UsuarioDAO extends GenericDAO<Usuario> {
+
+	public UsuarioDAO() {
+		super();
+	}
+
+	public List<Usuario> findAll() {
+		return findAll(Usuario.class);
+	}
+
+	public Usuario findByCodigo(String valor) {
+		return super.findByField(Usuario.class, "codigo", valor);
+	}
+
+	public Usuario findByLogin(String login){
+		return super.findByField(Usuario.class, "login", login);
+	}
+
+
+}
+
+package br.edu.ifpb.calendario.dao;
+
+import java.util.List;
+
+import br.edu.ifpb.calendario.models.Usuario;
+
+public class UsuarioDAO extends GenericDAO<Usuario> {
+
+	public UsuarioDAO() {
+		super();
+	}
+
+	public List<Usuario> findAll() {
+		return findAll(Usuario.class);
+	}
+
+	public Usuario findByCodigo(String valor) {
+		return super.findByField(Usuario.class, "codigo", valor);
+	}
+
+	public Usuario findByLogin(String login){
+		return super.findByField(Usuario.class, "login", login);
+	}
+
+
+}
+
+package br.edu.ifpb.calendario.dao;
+
+import java.util.List;
+
+import br.edu.ifpb.calendario.models.Usuario;
+
+public class UsuarioDAO extends GenericDAO<Usuario> {
+
+	public UsuarioDAO() {
+		super();
+	}
+
+	public List<Usuario> findAll() {
+		return findAll(Usuario.class);
+	}
+
+	public Usuario findByCodigo(String valor) {
+		return super.findByField(Usuario.class, "codigo", valor);
+	}
+
+	public Usuario findByLogin(String login){
+		return super.findByField(Usuario.class, "login", login);
+	}
+
+
+}
